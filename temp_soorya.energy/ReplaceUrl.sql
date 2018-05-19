33fa9992e3e18991be347abf0f59624d2faca210
trinixid <support@trinix.id>
2018-05-19 10:06:45 +0700

    Remove temp_soorya.energy

--- temp_soorya.energy/ReplaceUrl.sql
+++
@@ -1,2 +0,0 @@
-UPDATE MsMenu SET URL = REPLACE(Url, '10.7.5.176', 'localhost') WHERE Url LIKE '%10.7.5.176%'
-UPDATE MsMenu SET URL = REPLACE(Url, 'localhost', '10.7.5.176') WHERE Url LIKE '%localhost%'
